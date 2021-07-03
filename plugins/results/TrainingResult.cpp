625a90dc4fad08614b80a9d35d03843250d0a748
Paul S <uqmvi@student.kit.edu>
2021-07-03 19:05:59 +0200

    Remove dummy implementations and fill 'Results' with actual code

--- plugins/results/TrainingResult.cpp
+++ plugins/results/TrainingResult.cpp
@@ -1,23 +1,39 @@
 #include "TrainingResult.h"
 
-TrainingResult::TrainingResult(QList<QImage> additionalResult, QAbstractTableModel *confusionMatrix,
-                               QMap<int, QVector<double>> lossCurve, double top1Accuracy, double top5Accuracy,
-                               QList<QImage> additionalResults) : Result(additionalResult) {
+#include <utility>
 
+TrainingResult::TrainingResult(QMap<int, QPair<double,double>> lossCurve,
+                               QAbstractTableModel *confusionMatrix, const QStringList& classLabels, QList<QImage> mostMisclassifiedImages,
+                               double top1Accuracy, double top5Accuracy, QList<QImage> additionalResults)
+        : Result(std::move(additionalResults)) {
+    m_lossCurve = std::move(lossCurve);
+    m_confusionMatrix = confusionMatrix;
+    m_classLabels = classLabels;
+    m_mostMisclassifiedImages = std::move(mostMisclassifiedImages);
+    m_top1Accuracy = top1Accuracy;
+    m_top5Accuracy = top5Accuracy;
+}
+
+QMap<int, QPair<double,double>> TrainingResult::getLossCurve() {
+    return m_lossCurve;
 }
 
 QAbstractTableModel *TrainingResult::getConfusionMatrix() {
-    return nullptr;
+    return m_confusionMatrix;
 }
 
-QMap<int, QVector<double>> TrainingResult::getLossCurve() {
-    return QMap<int, QVector<double>>();
+const QStringList &TrainingResult::getClassLabels() const {
+    return m_classLabels;
 }
 
-double TrainingResult::getTop1Accuracy() {
-    return 0;
+const QList<QImage> &TrainingResult::getMostMisclassifiedImages() const {
+    return m_mostMisclassifiedImages;
 }
 
-double TrainingResult::getTop5Accuracy() {
-    return 0;
+double TrainingResult::getTop1Accuracy() const {
+    return m_top1Accuracy;
+}
+
+double TrainingResult::getTop5Accuracy() const {
+    return m_top5Accuracy;
 }
