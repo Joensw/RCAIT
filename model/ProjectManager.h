4787f7bc81148a4d4671d6e31ce7eb58fd553769
Jonas Wille <jonas.wille@arcor.de>
2021-07-03 19:18:51 +0200

    filled ProjectManager with information from UML

--- model/ProjectManager.h
+++ model/ProjectManager.h
@@ -5,8 +5,36 @@
 #ifndef RCAIT_PROJECTMANAGER_H
 #define RCAIT_PROJECTMANAGER_H
 
+#include <QString>
+
 
 class ProjectManager {
+public:
+    ProjectManager();
+
+    void createNewProject(QString projectName);
+    void removeProject(QString projectName);
+    void loadProject(QString projectName);
+    QString getProjectPath();
+    QString getProjectTempDir();
+    QString getProjectDataSetDir();
+
+    //todo doesnt have header yet/doesnt exist yet
+    //void saveClassificationResult(ClasificationResult result);
+    //void saveTrainingsResult(ClasificationResult result);
+    //TrainingResult getTrainingsResult(Qstring modelResultName);
+    QStringList getNamesOfSavedTrainingReults();
+
+
+
+
+
+private:
+    QString projectPath;
+    QString projectPathTempDir;
+    QString projectPathDataSetDir;
+    QString projectName;
+
 
 };
 
