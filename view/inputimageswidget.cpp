c685d703a579049128cb7517baaca5a250ce7bc7
AdNe <uevhx@student.kit.edu>
2021-07-03 08:23:54 +0200

    Implemented first version of InputImagesWidget

--- view/inputimageswidget.cpp
+++ view/inputimageswidget.cpp
@@ -1,6 +1,9 @@
 #include "inputimageswidget.h"
 #include "ui_inputimageswidget.h"
 
+#include <QFileDialog>
+
+
 InputImagesWidget::InputImagesWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::InputImagesWidget)
@@ -13,10 +16,73 @@ InputImagesWidget::~InputImagesWidget()
     delete ui;
 }
 
-void InputImagesWidget::slot_loaded(QString path) {
-
+void InputImagesWidget::slot_progress(int progress)
+{
+    ui->progressBar->setValue(progress);
 }
 
-void InputImagesWidget::slot_progress(int progress) {
-
+/*!
+ * \brief InputImagesWidget::on_selectFolderButton_clicked
+ *
+ * Used when selectFolderButton is clicked.
+ * Opens a file dialog and loads images of folder in preview.
+ */
+void InputImagesWidget::on_selectFolderButton_clicked()
+{
+     path = QFileDialog::getExistingDirectory(this, "Select image directory");
+     if (path == nullptr) return;
+     ui->preview->setEnabled(true);
+     loadDir(QDir(path));
+     ui->classifyButton->setEnabled(true);
 }
+
+
+
+void InputImagesWidget::loadDir(const QDir& imageDirectory) {
+    QStringList images = imageDirectory.entryList(QStringList() << "*.JPG" << "*.jpg" << "*.png", QDir::Files);
+
+
+            foreach(QString imageName, images) {
+
+            QString path = imageDirectory.path() + "/" + imageName;
+
+            QImage image(path);
+
+            int squareSize = image.height() < image.width() ? image.height() : image.width();
+            int leftX = image.width() / 2 - squareSize / 2;
+            int leftY = image.height() / 2 - squareSize / 2;
+
+
+            QImage copy = image.copy(leftX, leftY, squareSize, squareSize);
+            QListWidgetItem *item = new QListWidgetItem();
+            QPixmap tempImage = QPixmap::fromImage(copy);
+            item->setData(Qt::DecorationRole, tempImage.scaled(200, 200, Qt::KeepAspectRatio));
+
+
+            ui->preview->addItem(item);
+        }
+}
+
+
+void InputImagesWidget::on_classifyButton_clicked()
+{
+    ui->classifyButton->setEnabled(false);
+    ui->progressBar->setEnabled(true);
+    ui->selectFolderButton->setEnabled(false);
+    ui->cancelButton->setEnabled(true);
+
+
+    emit sig_startClassify(path);
+}
+
+
+void InputImagesWidget::on_cancelButton_clicked()
+{
+    emit sig_abortClassify();
+
+    ui->classifyButton->setEnabled(true);
+    ui->progressBar->setEnabled(true);
+    ui->selectFolderButton->setEnabled(true);
+    ui->cancelButton->setEnabled(false);
+}
+
--- view/inputimageswidget.h
+++ view/inputimageswidget.h
@@ -1,6 +1,7 @@
 #ifndef INPUTIMAGESWIDGET_H
 #define INPUTIMAGESWIDGET_H
 
+#include <QDir>
 #include <QWidget>
 
 namespace Ui {
@@ -20,13 +21,23 @@ signals:
     void sig_startClassify(QString path);
     void sig_abortClassify();
 
-public slots:
     void slot_loaded(QString path);
     void slot_progress(int progress);
+
+private slots:
+    void on_selectFolderButton_clicked();
+
+    void on_classifyButton_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_cancelButton_clicked();
 
 private:
     Ui::InputImagesWidget *ui;
     QString path;
+
+    void loadDir(const QDir &imageDirectory);
 };
 
 #endif // INPUTIMAGESWIDGET_H
