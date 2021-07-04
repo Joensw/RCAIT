04f8fe8ab2094e4504e53e96df42bd211ea7c9fc
Jonas Wille <jonas.wille@arcor.de>
2021-07-04 13:06:30 +0200

    First implementation of createNewProject method

--- model/ProjectManager.cpp
+++ model/ProjectManager.cpp
@@ -4,33 +4,57 @@
 
 #include "ProjectManager.h"
 #include <QDir>
+#include <QSettings>
 
 ProjectManager::ProjectManager(){
 
 }
 
-//Legt neue Projektdatei an, im projekt ordner in seinem eigenen Verzeichnis
 
 void ProjectManager::createNewProject(QString projectName){
-/*
-    //CHECK NAME UNIQUE;
+    //NOTE: the projects directory will also have to come from somewhere,
+    //currently it is created one directory up from the build directory.
 
-    QString path("projects" + "/" + projectName);
-    QDir dir;
 
-    if (!dir.exists(path)) {
-        dir.mkpath(path);
+    //These will actually be defined elsewhere, such as the settings Manager
+    const QString datasetDirName = "data";
+    const QString tempDirName = "temp";
+
+    //get list of all folders in project directory
+    QDir projectsDir("../projects");
+    projectsDir.setFilter(QDir::AllDirs | QDir::NoDotAndDotDot);
+    QStringList projects = projectsDir.entryList();
+
+    if (projects.contains(projectName)) {
+        //Do something, this is not allowed
+        //qDebug() << "Duplicate entry!";
+        //return;
     }
 
-    QFile file(path + projectName + "/" + "projectName");
-    file.open(QIODevice::WriteOnly);
 
-    //Parse n stuff
+    QString path = "../projects/" + projectName + "/" + projectName + ".ini";
 
-    WORK IN PROGRESS
-*/
+
+
+    /* goes into the projects folder, then into the specific project folder
+     * there it creates a new ini file, which can be seen as the "project file"
+     */
+
+
+    QSettings newProjectfile(path, QSettings::IniFormat);
+
+    //this is only to get an absolute path, without .. and .
+    QDir projectDir(QFileInfo(path).absoluteDir());
+    QString absolute = projectDir.absolutePath();
+
+    newProjectfile.setValue("projectName", projectName);
+    newProjectfile.setValue("projectDir", absolute);
+    newProjectfile.setValue("datasetDirName", datasetDirName);
+    newProjectfile.setValue("tempDirName", tempDirName);
+
+    //non existing folders / directories will be automatically created
 }
-//Lösche Projektdatei, ordner
+//Lösche Projektdatei ordner
 void ProjectManager::removeProject(QString projectName){
 
 }
