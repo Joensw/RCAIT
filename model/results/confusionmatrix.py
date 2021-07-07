b6c804f1d5f0174c9adfed64d75a4cb1c1e59962
Paul S <uqmvi@student.kit.edu>
2021-07-07 20:24:22 +0200

    Add option to produce normalized/non- normalized matrix

--- model/results/confusionmatrix.py
+++ model/results/confusionmatrix.py
@@ -10,7 +10,13 @@ import numpy as np
 plt.ioff()
 
 
-def plot_confusion_matrix(cm, lbl, file, cmap=plt.cm.get_cmap('CMRmap_r')):
+def plot_confusion_matrix(cm, lbl, file, normalize=False, cmap=plt.cm.get_cmap('CMRmap_r')):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.colorbar()
     plt.xticks(range(len(lbl)), lbl, rotation=45)
@@ -32,17 +38,20 @@ def plot_confusion_matrix(cm, lbl, file, cmap=plt.cm.get_cmap('CMRmap_r')):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generates a confusion matrix and stores it as an image.')
-    parser.add_argument('matrixdata', metavar='data',
+    parser.add_argument('matrixdata', metavar='data', required=True,
                         help='Raw number content of the matrix, formatted as a python array')
-    parser.add_argument('matrixlabels', metavar='labels',
+    parser.add_argument('matrixlabels', metavar='labels', required=True,
                         help='List of class labels, formatted as python array')
-    parser.add_argument('outfilename', metavar='outfile',
+    parser.add_argument('outfilename', metavar='outfile', required=True,
                         help='Output file name including extension')
+    parser.add_argument('normalized', type=bool, metavar='bool', required=False,
+                        help='Whether to produce a normalized matrix or not')
 
     args = parser.parse_args()
     matrix = np.array(ast.literal_eval(args.matrixdata))
     labels = ast.literal_eval(args.matrixlabels)
     file_name = args.outfilename
+    normalized = args.normalized
 
     np.set_printoptions(precision=1)
 
@@ -50,7 +59,5 @@ if __name__ == '__main__':
     plt.rcParams['font.family'] = 'sans-serif'
     plt.rcParams['font.sans-serif'] = 'SF Pro Text'
 
-    print('Confusion matrix, without normalization')
-    print(matrix)
-    plot_confusion_matrix(matrix, labels, file_name)
+    plot_confusion_matrix(matrix, labels, file_name, normalized)
     sys.exit()
