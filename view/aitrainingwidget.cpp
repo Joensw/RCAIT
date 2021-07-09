d89f02f7889233cfe341be4ff55ee5efab87c053
AdNe <uevhx@student.kit.edu>
2021-07-09 20:00:03 +0200

    Defined methods and slots in AITrainingWidget

--- view/aitrainingwidget.cpp
+++ view/aitrainingwidget.cpp
@@ -1,6 +1,9 @@
 #include "aitrainingwidget.h"
 #include "ui_aitrainingwidget.h"
 
+#include <QDoubleValidator>
+#include <QPair>
+
 AITrainingWidget::AITrainingWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::AITrainingWidget)
@@ -12,3 +15,71 @@ AITrainingWidget::~AITrainingWidget()
 {
     delete ui;
 }
+
+void AITrainingWidget::slot_progress(int progress)
+{
+    ui->progressBar->setValue(progress);
+    if (progress == 100){
+        ui->resultsButton->setEnabled(true);
+    }
+}
+
+void AITrainingWidget::on_startButton_clicked()
+{
+    emit sig_startTraining();
+    ui->cancelButton->setEnabled(true);
+    ui->progressBar->setEnabled(true);
+    ui->startButton->setEnabled(false);
+}
+
+
+void AITrainingWidget::on_cancelButton_clicked()
+{
+    emit sig_abortTraining();
+    ui->startButton->setEnabled(true);
+    ui->cancelButton->setEnabled(false);
+}
+
+
+void AITrainingWidget::on_resultsButton_clicked()
+{
+    emit sig_results();
+}
+
+int AITrainingWidget::getCropSize()
+{
+    return ui->resizedCropSize->value();
+}
+
+QString AITrainingWidget::getFlipDirection()
+{
+    return ui->flipDirection->text();
+}
+
+double AITrainingWidget::getFlipProbability()
+{
+    return ui->flipProbability->value();
+}
+
+QVector<QPair<double, double>> AITrainingWidget::getNormalizationTable()
+{
+    QVector<QPair<double, double>> normalizationTable(3, QPair<double, double>(ui->red_mean->value(), ui->red_std->value()));
+    normalizationTable.append(QPair<double, double>(ui->green_mean->value(), ui->green_std->value()));
+    normalizationTable.append(QPair<double, double>(ui->blue_mean->value(), ui->blue_std->value()));
+    return normalizationTable;
+}
+
+int AITrainingWidget::getResizeValue()
+{
+    return ui->resize->value();
+}
+
+
+
+
+
+void AITrainingWidget::on_previewButton_clicked()
+{
+    emit sig_showAugmentationPreview();
+}
+
--- view/aitrainingwidget.h
+++ view/aitrainingwidget.h
@@ -15,6 +15,29 @@ public:
     explicit AITrainingWidget(QWidget *parent = nullptr);
     ~AITrainingWidget();
 
+    int getCropSize();
+    QString getFlipDirection();
+    QVector<QPair<double, double>> getNormalizationTable();
+    double getFlipProbability();
+    int getResizeValue();
+
+
+public slots:
+    void slot_progress(int progress);
+
+signals:
+    void sig_showAugmentationPreview();
+    void sig_startTraining();
+    void sig_abortTraining();
+    void sig_results();
+
+private slots:
+    void on_startButton_clicked();
+
+    void on_cancelButton_clicked();
+
+    void on_resultsButton_clicked();
+
 private:
     Ui::AITrainingWidget *ui;
 };
