8606e56b6d548a54a9d1df6cda52b81d033b7d12
AdNe <uevhx@student.kit.edu>
2021-07-10 17:48:59 +0200

    Implemented some buttons of AutomationWidget

--- view/automationwidget.cpp
+++ view/automationwidget.cpp
@@ -12,3 +12,65 @@ AutomationWidget::~AutomationWidget()
 {
     delete ui;
 }
+
+void AutomationWidget::on_startButton_clicked()
+{
+    emit sig_start();
+    ui->stopButton->setEnabled(true);
+    ui->startButton->setEnabled(false);
+}
+
+
+void AutomationWidget::on_stopButton_clicked()
+{
+    emit sig_stop();
+    ui->startButton->setEnabled(true);
+    ui->stopButton->setEnabled(false);
+}
+
+
+void AutomationWidget::on_removeButton_clicked()
+{
+    for (QModelIndex index : ui->idleTasks->selectionModel()->selectedIndexes()){
+        emit sig_remove(index.row());
+    }
+}
+
+
+void AutomationWidget::on_unqueueSelectedButton_clicked()
+{
+    for (QModelIndex index : ui->queuedTasks->selectionModel()->selectedIndexes()){
+        emit sig_unqueueSelected(index.row());
+    }
+}
+
+
+void AutomationWidget::on_queueSelectedButton_clicked()
+{
+    for (QModelIndex index : ui->idleTasks->selectionModel()->selectedIndexes()){
+        emit sig_queueSelected(index.row());
+    }
+}
+
+
+void AutomationWidget::on_queueAllButton_clicked()
+{
+    int size = ui->idleTasks->count();
+
+    for (int i = 0; i < size; i++){
+        ui->queuedTasks->addItem(ui->idleTasks->item(i)->text());
+        ui->idleTasks->removeItemWidget(ui->idleTasks->item(i));
+    }
+}
+
+
+void AutomationWidget::on_unqueueAllButton_clicked()
+{
+    int size = ui->queuedTasks->count();
+
+    for (int i = 0; i < size; i++){
+        ui->idleTasks->addItem(ui->queuedTasks->item(i)->text());
+        ui->queuedTasks->removeItemWidget(ui->idleTasks->item(i));
+    }
+}
+
--- view/automationwidget.h
+++ view/automationwidget.h
@@ -15,6 +15,28 @@ public:
     explicit AutomationWidget(QWidget *parent = nullptr);
     ~AutomationWidget();
 
+public slots:
+    void slot_progress(int progress);
+
+
+signals:
+    void sig_start();
+    void sig_stop();
+    void sig_remove(int index);
+    void sig_import(QString path);
+    void sig_queueAll();
+    void sig_unqueueAll();
+    void sig_queueSelected(int index);
+    void sig_unqueueSelected(int index);
+
+
+private slots:
+    void on_startButton_clicked();
+
+    void on_stopButton_clicked();
+
+    void on_removeButton_clicked();
+
 private:
     Ui::AutomationWidget *ui;
 };
