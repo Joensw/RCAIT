1f1a2f74598f2f6cf6022adc8c8cb6d0c700af80
Jonas Wille <jonas.wille@arcor.de>
2021-07-11 16:52:03 +0200

    updated the modelDialog and connected them with the importfiles widget

--- view/removemodeldialog.h
+++ view/removemodeldialog.h
@@ -12,11 +12,22 @@ class RemoveModelDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit RemoveModelDialog(QWidget *parent = nullptr);
+    explicit RemoveModelDialog(QWidget *parent = nullptr, QString toRemove = "");
     ~RemoveModelDialog();
+
+    QString getModelName();
+
+signals:
+    void sig_removeModelConfirm();
+
 
 private:
     Ui::RemoveModelDialog *ui;
+
+    QString mToRemove;
+private slots:
+    void on_buttonBox_accepted();
+    void on_buttonBox_rejected();
 };
 
 #endif // REMOVEMODELDIALOG_H
