3c7ee7009a7499c60741edd3fe9ff4bc7d1e34c1
SaschaRolinger <60555506+SaschaRolinger@users.noreply.github.com>
2021-07-19 21:28:13 +0200

    Flickrplugin is now build with project and included with cmake build

---
+++ plugins/imageplugins/flickrplugin/flickrplugin.cpp
@@ -0,0 +1,73 @@
+#include "flickrplugin.h"
+
+
+bool FlickrPlugin::loadImages(const QString path,ProgressablePlugin* receiver, const int imageCount, const QStringList label)
+{
+
+
+
+    QString fullCommand = createCommandlineString(path, imageCount, &label);
+    QProcess *process = new QProcess();
+    process->startCommand(fullCommand);
+
+    process->waitForStarted();
+    bool res = process->waitForFinished();
+
+    QString strTemp = QString::fromLocal8Bit(process->readAll());  // Get the output
+    qInfo() << qPrintable(strTemp.simplified());
+
+    process->close();
+
+    return res;
+}
+
+
+QString FlickrPlugin::createCommandlineString(const QString path, const int imageCount, const QStringList* label){
+    //get and format command line parameters for python script call
+    QString downloadPath = QString("-p ").append(path);
+    QString imageCountStr = QString("-c ").append(QString::number(imageCount));
+    QFileInfo pythonfile = QFileInfo("flickrapi_photosearch.py");
+
+    QString scriptPath = pythonfile.absoluteFilePath();
+    QString command = m_flickrSettings.getPythonPath();
+    QString apiKey = QString("-k ").append(m_flickrSettings.getaAPIKey());
+    QString apiSecret = QString("-s ").append(m_flickrSettings.getAPISecret());
+    QString labelConcat = "-l";
+
+    for ( const auto& i : *label  )
+    {
+        labelConcat.append(" ");
+        labelConcat.append(i);
+    }
+
+    QString fullCommand = command +  " " + scriptPath + " " + downloadPath  + " " +imageCountStr +" " + labelConcat +" " + apiKey +" " + apiSecret;
+    return fullCommand;
+
+}
+
+
+QWidget* FlickrPlugin::getConfigurationWidget()
+{
+  return pluginSettings;
+}
+
+void FlickrPlugin::saveConfiguration(){
+    qobject_cast<FlickrSettings *>(pluginSettings)->saveSettings();
+}
+
+void FlickrPlugin::init()
+{
+    pluginSettings = new FlickrSettings();
+}
+
+QString FlickrPlugin::getName()
+{
+    return "Flickr API Plugin";
+}
+
+QWidget *FlickrPlugin::getInputWidget()
+{
+    return nullptr;
+}
+
+//! [0]
---
+++ plugins/imageplugins/flickrplugin/flickrsettings.h
@@ -0,0 +1,36 @@
+#ifndef FLICKRSETTINGS_H
+#define FLICKRSETTINGS_H
+
+#include <QWidget>
+#include <QSettings>
+#include <QtCore>
+
+namespace Ui {
+class FlickrSettings;
+}
+
+class FlickrSettings : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit FlickrSettings(QWidget *parent = nullptr);
+    ~FlickrSettings();
+    void saveSettings();
+    void loadSettings();
+    QString getaAPIKey();
+    QString getAPISecret();
+    QString getPythonPath();
+
+
+    QSettings m_settings = {"Flickr Plugin'",QSettings::IniFormat};
+
+
+private:
+    Ui::FlickrSettings *ui;
+    QString m_apiKey = "API Key";
+    QString m_apiSecret = "API Secret";
+    QString m_pythonPath = "Python Path";
+};
+
+#endif // FLICKRSETTINGS_H
