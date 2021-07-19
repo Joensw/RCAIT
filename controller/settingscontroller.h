b8bf64b390735ebbd87596f56ceb6797eb3fef93
AdNe <uevhx@student.kit.edu>
2021-07-19 18:48:06 +0200

    Documentation of SettingsController

--- controller/settingscontroller.h
+++ controller/settingscontroller.h
@@ -9,23 +9,53 @@
 
 
 
-
+/**
+ * @brief The SettingsController class mediates between the settings logic and settings UI.
+ *
+ */
 class SettingsController : public QObject
 {
     Q_OBJECT
 public:
-    // SettingsController besorgt sich ja erst alle Informationen, die nötig sind um ein (sinvolles) SettingsView zu erstellen,
-    // daher wird kein SettingsView mitgegeben sondern erst hier erzeugt
+
+    /**
+     * @brief SettingsController constructs a SettingsController with the given parameters.
+     *
+     * @param parent optional parent object.
+     * @param dataManager source of general information.
+     */
     explicit SettingsController(QObject *parent = nullptr, DataManager *dataManager = nullptr);
 
 
 public slots:
+
+    /**
+     * @brief slot_openSettings opens a SettingView when triggered.
+     *
+     */
     void slot_openSettings();
 
+    /**
+     * @brief slot_applySettings aplies settings of plugin at index.
+     *
+     * @param index position of plugin settings widget.
+     */
     void slot_applySettings(int index);
+
+    /**
+     * @brief slot_applyGlobalSettings applies global settings.
+     * @param projectDir projectsDir selected project directory.
+     * @param classificationPluginDir selected classification plugin directory.
+     * @param imageLoaderPluginsDir selected imageloader plugin directory.
+     */
     void slot_applyGlobalSettings(QString projectDir, QString classificationPluginDir, QString imageLoaderPluginsDir);
 
 signals:
+
+    /**
+     * @brief sig_projectDirectoryChanged emitted when project directory is changed.
+     *
+     */
     void sig_projectDirectoryChanged();
 
 private:
