db0a9d6cbf8b65674183d3a0ffb795c4192a24a5
AdNe <uevhx@student.kit.edu>
2021-07-22 11:21:38 +0200

    Documentation of Automator

--- model/automation/automator.h
+++ model/automation/automator.h
@@ -8,20 +8,74 @@
 #include <pluginusage/progressable.h>
 
 
-
+/**
+ * @brief The Automator class for managing task scheduling and execution.
+ *
+ */
 class Automator : public Progressable
 {
     Q_OBJECT
 public:
+
+    /**
+     * @brief Automator constructs a new Automator.
+     *
+     * @param dataManager source of general information.
+     */
     Automator(DataManager *dataManager);
 
+    /**
+     * @brief performTasks starts tasks scheduled for execution.
+     *
+     */
     void performTasks();
+
+    /**
+     * @brief stopTasks stops execution of tasks.
+     *
+     */
+    void stopTasks();
+
+    /**
+     * @brief addTasks tries adding a task defined in file.
+     *
+     * @param path path to file.
+     */
     void addTasks(QString path);
+
+    /**
+     * @brief remove removes task at index from non-scheduled queue.
+     *
+     * @param taskNum index of task.
+     */
     void remove(int taskNum);
+
+    /**
+     * @brief unqueue unqueues task at index from scheduled queue.
+     *
+     * @param taskNum index of task.
+     */
     void unqueue(int taskNum);
+
+    /**
+     * @brief queue queues task at index from non-scheduled queue.
+     *
+     * @param taskNum index of task.
+     */
     void queue(int taskNum);
 
+    /**
+     * @brief getUnqueuedSize returns size of non-scheduled queue.
+     *
+     * @return size of non-scheduled queue.
+     */
     int getUnqueuedSize();
+
+    /**
+     * @brief getQueuedSize returns size of scheduled queue.
+     *
+     * @return size of scheduled queue.
+     */
     int getQueuedSize();
 
     //probably never used
@@ -30,15 +84,47 @@ public:
     QList<QString> getQueuedTasks();
 
 public slots:
+
+    /**
+     * @brief slot_makeProgress defines behavior when accepting progress signals.
+     *
+     * @param progress number in percent.
+     */
     void slot_makeProgress(int progress) override;
 
+    /**
+     * @brief slot_taskUpdated is used to handle changed task states.
+     *
+     * @param name task name.
+     * @param state task state.
+     */
     void slot_taskUpdated(QString name, TaskState state);
-    void stopTasks();
+
+
 
 signals:
+
+    /**
+     * @brief sig_taskUpdate signals changed task to observers.
+     *
+     * @param name task name.
+     * @param state task state.
+     */
     void sig_taskUpdate(QString name, QString state);
+
+    /**
+     * @brief sig_taskAdded signals successfull addition of task to non-scheduled queue.
+     *
+     * @param name task name.
+     */
     void sig_taskAdded(QString name);
-    void sig_progress(int);
+
+    /**
+     * @brief sig_progress signals progress of batch processing.
+     *
+     * @param progress number in percent
+     */
+    void sig_progress(int progress);
 
 
 private:
