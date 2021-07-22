6589efb1ff09d9262b6ff2f6416cd448eb0c1d41
Jonas Wille <jonas.wille@arcor.de>
2021-07-22 15:27:45 +0200

    documentation configuartionDialog

--- view/configurationdialog.h
+++ view/configurationdialog.h
@@ -11,6 +11,11 @@ namespace Ui {
 class ConfigurationDialog;
 }
 
+/**
+ * The configuration UI will appear on first time application use and will also appear subsequently if the set paths are not resolvable anymore
+ * such as when the underlying folders are deleted
+ * @brief The ConfigurationDialog class is used to insure programm critical paths are set before continuing on startup
+ */
 class ConfigurationDialog : public QDialog
 {
     Q_OBJECT
@@ -18,15 +23,39 @@ class ConfigurationDialog : public QDialog
 public:
     explicit ConfigurationDialog(QWidget *parent = nullptr);
     ~ConfigurationDialog();
+
+    /**
+     * @brief closeEvent closes the dialog on a close event, if it has accepted the input, shows a quit UI otherwise
+     * @param event close event
+     */
     virtual void closeEvent(QCloseEvent *event) override;
+
+    /**
+     * @brief reject a reject event is treated as a close call
+     */
     virtual void reject() override;
 
-    void setError(QString error);
+    /**
+     * @brief showError lets the standardized error message in the configuration UI appear
+     */
+    void showError();
+
+    /**
+     * @brief confirm prompts the configuration UI to be closed
+     */
     void confirm();
 
 signals:
+
+    /**
+     * Ports the new paths into the application if they are acceptable, shows an error message in the UI otherwise
+     * @brief sig_directoriesSpecified emitted when the input paths are to be checked for validity
+     * @param projectsDir the specified projects directory
+     * @param classificationPluginsDir the specifed classification plugin directory
+     * @param imageLoaderPluginsDir the specifed image loader plugin directory
+     */
     void sig_directoriesSpecified(QString projectsDir, QString classificationPluginsDir, QString imageLoaderPluginsDir);
-    void sig_forceClose();
+
 private:
     Ui::ConfigurationDialog *ui;
 
@@ -36,10 +65,24 @@ private:
 
     bool mIsCloseable;
 private slots:
+    /**
+     * @brief on_pushButton_apply_clicked emits the {sig_directories} signal when the "OK" button in the UI is pressed
+     */
     void on_pushButton_apply_clicked();
 
+    /**
+     * @brief slot_setProjectDir shows the user selected projects directory path from a file dialog in the UI
+     */
     void slot_setProjectDir();
+
+    /**
+     * @brief slot_setClassificationPluginsDir shows the user selected classification plugin directory path from a file dialog in the UI
+     */
     void slot_setClassificationPluginsDir();
+
+    /**
+     * @brief slot_setImageLoaderPluginsDir shows the user selected image loader plugin directory path from a file dialog in the UI
+     */
     void slot_setImageLoaderPluginsDir();
 };
 
