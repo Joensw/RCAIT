d50a0bde7b4c1ad8ea4b0dd920be3c16f23c036f
AdNe <uevhx@student.kit.edu>
2021-07-23 10:45:24 +0200

    Documentation for ImageLoadCommand

--- model/automation/imageloadcommand.h
+++ model/automation/imageloadcommand.h
@@ -6,11 +6,27 @@
 #include <imagesearchthread.h>
 
 
-
+/**
+ * @brief The ImageLoadCommand class starts image loading with information derived from a map.
+ *
+ */
 class ImageLoadCommand : public Command
 {
 public:
+
+    /**
+     * @brief ImageLoadCommand constructs a ImageLoadCommand by parsing from a map.
+     *
+     * @param map contains necessary information for command.
+     * @param receiver object to receive progress.
+     */
     ImageLoadCommand(QVariantMap map, Progressable* receiver);
+
+    /**
+     * @brief execute executes the command.
+     *
+     * @return true if execution was possible (no parsing error).
+     */
     bool execute() override;
 
 private:
