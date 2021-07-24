7d291f0f5f28fc6d6b03eb469bd5b7dde992ae2a
AdNe <uevhx@student.kit.edu>
2021-07-24 23:12:04 +0200

    Uses icons for task states in AutomationWidget

--- model/automation/automator.cpp
+++ model/automation/automator.cpp
@@ -48,7 +48,7 @@ void Automator::addTasks(QString path)
     if (!task->isValid()) return;
     mUnqueuedTasks.append(task);
     emit sig_taskAdded(jsonMap.value("taskName").toString());
-
+    emit sig_taskUpdate(mUnqueuedTasks.last()->getName(), "Not_Scheduled");
 }
 
 void Automator::remove(int taskNum)
@@ -60,12 +60,14 @@ void Automator::unqueue(int taskNum)
 {
     mUnqueuedTasks.append(mQueuedTasks.at(taskNum));
     mQueuedTasks.remove(taskNum);
+    emit sig_taskUpdate(mUnqueuedTasks.last()->getName(), "Not_Scheduled");
 }
 
 void Automator::queue(int taskNum)
 {
     mQueuedTasks.append(mUnqueuedTasks.at(taskNum));
     mUnqueuedTasks.remove(taskNum);
+    emit sig_taskUpdate(mQueuedTasks.last()->getName(), "Scheduled");
 }
 
 int Automator::getUnqueuedSize()
