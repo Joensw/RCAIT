0417ec5b1f89a6d6d0c85a7d7518638aa395d7d2
Paul S <uqmvi@student.kit.edu>
2021-07-29 15:50:55 +0200

    Disabled tabs are now shown in grey looking colors

--- view/customtabbar.cpp
+++ view/customtabbar.cpp
@@ -17,13 +17,25 @@ void CustomTabBar::paintEvent(QPaintEvent *e) {
         QPixmap pixmap = option.icon.pixmap(option.iconSize);
         int height = option.rect.height();
 
+        //Paint basic tab shape
         painter.drawControl(QStyle::CE_TabBarTabShape, option);
+
         // Draw the selected tab to get it "on top"
         auto offset = (i == selected) ? SELECTED_OFFSET(height) : OFFSET(height);
         option.rect.moveTop((int) offset);
         //Cut upper/lower parts of rectangle so the content is more centered
         option.rect.setHeight((int) (height - 2 * OFFSET(height)));
+
+        //Paint tab in grey colors if it is currently disabled
+        auto colorRole = QPalette::NoRole;
+        if (!m_tabWidget->isTabEnabled(i)) {
+            //Generate tab icon in disabled state and set tab name color to a gray-ish one
+            pixmap = painter.style()->generatedIconPixmap(QIcon::Disabled, pixmap, &option);
+            colorRole = QPalette::PlaceholderText;
+        }
+
+        //Finally, draw configured icon and text
         painter.drawItemPixmap(option.rect, Qt::AlignTop | Qt::AlignHCenter, pixmap);
-        painter.drawItemText(option.rect, Qt::AlignBottom | Qt::AlignHCenter, option.palette, true, option.text);
+        painter.drawItemText(option.rect, Qt::AlignBottom | Qt::AlignHCenter, option.palette, true, option.text, colorRole);
     }
 }
