9395f552e68e28f6c7b4bf4f4b4a392ed77e27a8
Paul S <uqmvi@student.kit.edu>
2021-07-31 19:39:03 +0200

    Update descriptions

--- model/results/topaccuraciesgraphics.cpp
+++ model/results/topaccuraciesgraphics.cpp
@@ -15,7 +15,7 @@ QString TopAccuraciesGraphics::valuesToPyText() {
 
         //List for each row, which shall be joined in a single QString
         auto rowList = QStringList();
-        for (const auto &value : valuesList){
+        for (const auto &value : valuesList) {
             //Convert to QString with precision of 2 digits
             auto valStr = QString::number(value, 'G', 5);
             rowList << valStr;
@@ -41,8 +41,8 @@ void TopAccuraciesGraphics::addDataRow(const QString &identifier, const QList<do
 
 void TopAccuraciesGraphics::removeDataRow(const QString &identifier) {
     auto index = 0;
-    for (const auto &[key, _] : m_data){
-        if (key == identifier){
+    for (const auto &[key, _] : m_data) {
+        if (key == identifier) {
             m_data.remove(index);
             return;
         }
@@ -51,7 +51,7 @@ void TopAccuraciesGraphics::removeDataRow(const QString &identifier) {
 }
 
 QList<double> TopAccuraciesGraphics::operator[](const QString &identifier) const {
-    for (const auto &[key, valuesList] : m_data){
+    for (const auto &[key, valuesList] : m_data) {
         if (key == identifier)
             return valuesList;
     }
@@ -71,10 +71,10 @@ bool TopAccuraciesGraphics::operator!=(const TopAccuraciesGraphics &other) const
 }
 
 void TopAccuraciesGraphics::generateGraphicsInternal(const QString &fullFilePath) {
-    //TODO Update description
-    // python script.py <loss curve data> <output file name>
+    // python script.py <top acc data> <top acc row labels> <output file name>
     auto pyScript = QFileInfo("topaccuraciesgraphics.py");
-    QStringList params = QStringList() << pyScript.absoluteFilePath() << valuesToPyText() << labelsToPyText() <<fullFilePath;
+    QStringList params =
+            QStringList() << pyScript.absoluteFilePath() << valuesToPyText() << labelsToPyText() << fullFilePath;
     AbstractResultGraphics::launch_externalGraphicsGenerator("python", params);
 }
 
