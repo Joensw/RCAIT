3f91f64cf120b209a57de9632b6cc8c365d69e8b
Paul S <uqmvi@student.kit.edu>
2021-08-01 20:01:00 +0200

    Add saved attribute for graphics views

--- view/results/abstractgraphicsview.h
+++ view/results/abstractgraphicsview.h
@@ -12,6 +12,13 @@ public:
     virtual void setTopAccuraciesGraphics(QGraphicsItem *topAccuraciesImage) {};
 
     virtual void setClassificationGraphics(QGraphicsItem *classificationGraphicsImage) {};
+
+    [[nodiscard]] bool isSaved() const { return m_isSaved; };
+
+    void setSaved(bool isSaved) { m_isSaved = isSaved; };
+
+private:
+    bool m_isSaved;
 };
 
 #endif // ABSTRACTGRAPHICSVIEW_H
