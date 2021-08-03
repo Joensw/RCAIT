2bf5965612548fbf4241295d94dfa6db7cd8cc02
AdNe <uevhx@student.kit.edu>
2021-08-03 22:37:01 +0200

    Fixed bug where plugins would not in automation part.

--- plugins/imageplugins/folderplugin/folderplugin.cpp
+++ plugins/imageplugins/folderplugin/folderplugin.cpp
@@ -5,33 +5,42 @@
 bool FolderPlugin::loadImages(const QString path, ProgressablePlugin* receiver, const int imageCount = 0, const QStringList label = QStringList())
 {
     QDir output(path);
-    QDir folder = QDir(mConfigWidget->getImageFolder());
-    if (!folder.exists() || !output.exists()) return false;
+    QDir folder = QDir(imageDir);
+    if (!folder.exists() || !output.exists()){
+        receiver->slot_makeProgress(100);
+        return false;
+    }
 
     switch(mode){
         case 0: {
-            QStringList imagefolders = folder.entryList(QDir::Dirs);
+            QStringList imagefolders = folder.entryList(QDir::AllDirs | QDir::NoDotAndDotDot);
             int i = 0;
             foreach(QString folderName, imagefolders){
-                if (folderName == "." || folderName == "..") continue;
                 folder.cd(folderName);
                 QStringList imagelist = folder.entryList(QStringList() << "*.JPG" << "*.jpg" << "*.png", QDir::Files);
                 if (!imagelist.isEmpty()){
-                    if (!addLabel(imagelist, folder, output)) return false;
+                    if (!addLabel(imagelist, folder, output)){
+                        receiver->slot_makeProgress(100);
+                        return false;
+                    }
                 }
                 folder.cdUp();
             }
+            receiver->slot_makeProgress(100);
             return true;
             break;
         }
 
         case 1: {
+            // syntax for labels by filename is "label_image.png"
             return false;
             break;
         }
         case 2: {
             QStringList images = folder.entryList(QStringList() << "*.JPG" << "*.jpg" << "*.png", QDir::Files);
-            return addLabel(images, folder, output);
+            bool success = addLabel(images, folder, output);
+            receiver->slot_makeProgress(100);
+            return success;
             break;
         }
 
@@ -48,6 +57,7 @@ QWidget *FolderPlugin::getConfigurationWidget()
 void FolderPlugin::saveConfiguration()
 {
     mode = mConfigWidget->getLoadMode();
+    imageDir = mConfigWidget->getImageFolder();
 }
 
 void FolderPlugin::init()
