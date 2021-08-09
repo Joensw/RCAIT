95c903cb7fadce57375bf3bd4317bf292fa06dc1
Paul S <uqmvi@student.kit.edu>
2021-08-09 18:14:37 +0200

    Allow empty additionalResults for classification/training results

--- plugins/results/trainingresult.h
+++ plugins/results/trainingresult.h
@@ -23,7 +23,7 @@ public:
     TrainingResult(const QMap<int, QPair<double, double>> &accuracyCurveData,
                    const QStringList &classLabels, const QList<int> &confusionMatrixValues,
                    QStringList mostMisclassifiedImages, double top1Accuracy, double top5Accuracy,
-                   const QStringList &additionalResults);
+                   const QStringList &additionalResults = {});
 
     [[nodiscard]] ConfusionMatrix *getConfusionMatrix() const;
 
