0bb4ba8f3cd1a599c238d21cd12608f140e382c4
Paul S <uqmvi@student.kit.edu>
2021-08-09 19:53:13 +0200

    Number rows of classification result table

--- view/results/classificationresultview.cpp
+++ view/results/classificationresultview.cpp
@@ -26,10 +26,10 @@ ClassificationResultView::ClassificationResultView(QWidget *parent) :
     table->setCornerButtonEnabled(false);
 }
 
-void ClassificationResultView::setClassificationData(const QMap<QString, QStringList> &data){
+void ClassificationResultView::setClassificationData(const QMap<int, QStringList> &data){
     auto table = ui->tableWidget_classificationresult;
     for (const auto &[key,valuesList] : MapAdapt(data)){
-        table->addTableRow(key,valuesList);
+        table->addTableRow(QString::number(key),valuesList);
     }
 }
 
