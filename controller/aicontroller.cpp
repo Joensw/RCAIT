7d692cba57b1d44ee87894f2f87d50b93bcd2790
Daria_Bidenko <hey.daria.bidenko@gmail.com>
2021-08-20 13:36:40 +0200

    Added a new Buttom for loading Keywords/Labels from txt File

--- controller/aicontroller.cpp
+++ controller/aicontroller.cpp
@@ -9,6 +9,12 @@ AIController::AIController(DataManager *dataManager, InputImagesWidget *inputIma
     mTrainer = new Trainer;
     mClassifier = new Classifier;
 
+
+    //connect augmentation preview
+    //mAItrainingWIdget sig_showAugmentationPreview         AI Controller slot_showAugmentationPreview
+
+
+
     //connect progress
     connect(mClassifier, &Classifier::sig_progress, mInputImagesWidget, &InputImagesWidget::slot_progress);
     connect(mTrainer, &Trainer::sig_progress, mAiTrainingWidget, &AITrainingWidget::slot_progress);
@@ -79,6 +85,8 @@ void AIController::slot_abortClassify()
 
 void AIController::slot_showAugmentationPreview()
 {
+//ToDo: Daria schreibt hier was passiert
+    //call bool ClassificationPluginManager::getAugmentationPreview(QString pluginName, QString modelName, QString inputPath, QString targetPath, int amount)
 
 }
 
