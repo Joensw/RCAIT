a7e16a726b82358ff787052ffa832dd7ecbf0c87
Paul S <uqmvi@student.kit.edu>
2021-08-22 16:52:37 +0200

    Remove useless class

--- plugins/imageplugins/imageloaderplugin.cpp
+++
@@ -1,6 +0,0 @@
-#include "imageloaderplugin.h"
-
-bool ImageLoaderPlugin::loadImages(QString path, ProgressablePlugin *receiver, int count,
-                                   QStringList labels) {
-    return false;
-}
