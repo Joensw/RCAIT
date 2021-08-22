788b527072ac9074914b1dd5b9bd0755497f74fe
Paul S <uqmvi@student.kit.edu>
2021-08-22 17:02:37 +0200

    Improved loading labels from file, added empty path check and translatable file dialogue title

--- view/importfileswidget.cpp
+++ view/importfileswidget.cpp
@@ -8,17 +8,15 @@
 #include <QMessageBox>
 
 ImportFilesWidget::ImportFilesWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::ImportFilesWidget)
-{
+        QWidget(parent),
+        ui(new Ui::ImportFilesWidget) {
     ui->setupUi(this);
-    ui->label_splitPercent->setText(QString::number(ui->horizontalSlider->value())+"%");
+    ui->label_splitPercent->setText(QString::number(ui->horizontalSlider->value()) + "%");
 
 
 }
 
-ImportFilesWidget::~ImportFilesWidget()
-{
+ImportFilesWidget::~ImportFilesWidget() {
     delete ui;
 }
 
@@ -57,23 +55,20 @@ void ImportFilesWidget::on_pushButton_clearTags_clicked() {
     ui->lineEdit_labels->tags(tags);
 }
 
-void ImportFilesWidget::on_pushButton_addModel_clicked()
-{
+void ImportFilesWidget::on_pushButton_addModel_clicked() {
     emit sig_newModel();
 }
 
-void ImportFilesWidget::on_pushButton_removeModel_clicked()
-{
-    QListWidgetItem * item = ui->listWidget_modelNames->currentItem();
+void ImportFilesWidget::on_pushButton_removeModel_clicked() {
+    QListWidgetItem *item = ui->listWidget_modelNames->currentItem();
     if (item) {
         QString toRemove = item->text();
         emit sig_removeModel(toRemove);
     }
 }
 
-void ImportFilesWidget::on_pushButton_loadModel_clicked()
-{
-    QListWidgetItem * item = ui->listWidget_modelNames->currentItem();
+void ImportFilesWidget::on_pushButton_loadModel_clicked() {
+    QListWidgetItem *item = ui->listWidget_modelNames->currentItem();
     if (item) {
         QString toLoad = item->text();
         emit sig_loadModel(toLoad);
@@ -81,17 +76,15 @@ void ImportFilesWidget::on_pushButton_loadModel_clicked()
 }
 
 
-
-
-void ImportFilesWidget::on_pushButton_loadImages_clicked()
-{
-    std::vector<QString> labelsVector =  ui->lineEdit_labels->tags();
+void ImportFilesWidget::on_pushButton_loadImages_clicked() {
+    std::vector<QString> labelsVector = ui->lineEdit_labels->tags();
     QStringList labelsList;
-    for( QString label : labelsVector){
+    for (QString label : labelsVector) {
         labelsList.append(label);
     }
 
-    emit sig_loadInputImages( ui->comboBox_plugins->currentText(),ui->spinBox_amount->value(),labelsList,ui->horizontalSlider->value());
+    emit sig_loadInputImages(ui->comboBox_plugins->currentText(), ui->spinBox_amount->value(), labelsList,
+                             ui->horizontalSlider->value());
 
 }
 
@@ -100,43 +93,38 @@ void ImportFilesWidget::setAvailablePlugins(QStringList pluginNames) {
     ui->comboBox_plugins->addItems(pluginNames);
 }
 
-void ImportFilesWidget::updateProgressBar(int progress)
-{
+void ImportFilesWidget::updateProgressBar(int progress) {
     ui->progressBar_images->setValue(progress);
 }
 
-void ImportFilesWidget::updateStatusText(QString status)
-{
+void ImportFilesWidget::updateStatusText(QString status) {
     QFont labelFont = ui->label_pluginStatus->font();
     QFontMetricsF labelFontMetrics(labelFont);
-    ui->label_pluginStatus->setText(labelFontMetrics.elidedText(status,Qt::TextElideMode::ElideRight, ui->label_pluginStatus->width()));
+    ui->label_pluginStatus->setText(
+            labelFontMetrics.elidedText(status, Qt::TextElideMode::ElideRight, ui->label_pluginStatus->width()));
 }
 
 
-
-
-
-void ImportFilesWidget::on_horizontalSlider_sliderMoved(int position)
-{
-     ui->label_splitPercent->setText(QString::number(position)+"%");
+void ImportFilesWidget::on_horizontalSlider_sliderMoved(int position) {
+    ui->label_splitPercent->setText(QString::number(position) + "%");
 }
 
 
+void ImportFilesWidget::on_pushButton_loadLabelsFromFile_clicked() {
+    std::vector<QString> labelsVector;
+    QString path = QFileDialog::getOpenFileName(this, tr("Select .txt file"), "", "*.txt");
+    if (path.isEmpty()) return;
 
-void ImportFilesWidget::on_pushButton_loadLabelsFromFile_clicked()
-{
-   std::vector<QString> labelsVector;
-   QString path = QFileDialog::getOpenFileName(this, "Select .txt file", "", "*.txt");
-   if (path == nullptr) return;
-   QFile inputFile(path);
-   inputFile.open(QIODevice::ReadOnly);
-   if (!inputFile.isOpen()) return;
-   QTextStream stream(&inputFile);
-   QString line = stream.readLine();
-   while (!line.isNull()) {
-       labelsVector.push_back(line.simplified());
-       line = stream.readLine();
-   };
-   ui->lineEdit_labels->tags(labelsVector);
+    QFile inputFile(path);
+    inputFile.open(QIODevice::ReadOnly);
+
+    if (!inputFile.isOpen()) return;
+    QTextStream stream(&inputFile);
+
+    for (auto line = stream.readLine(); !line.isNull(); line = stream.readLine()) {
+        labelsVector.push_back(line.simplified());
+    }
+
+    ui->lineEdit_labels->tags(labelsVector);
 }
 
