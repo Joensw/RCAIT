42beaa367dcea4f94943d50635bb4176db051e1c
Paul S <uqmvi@student.kit.edu>
2021-09-02 12:36:38 +0200

    Small optimizations, rearranged code

--- controller/resultscontroller.cpp
+++ controller/resultscontroller.cpp
@@ -1,28 +1,22 @@
 #include <resultsexporter.h>
 #include "resultscontroller.h"
 
-ResultsController::ResultsController(DataManager *manager, ResultsWidget *resultsWidget) {
-    m_dataManager = manager;
-    m_resultsWidget = resultsWidget;
+ResultsController::ResultsController(DataManager *manager, ResultsWidget *resultsWidget)
+        : m_dataManager(manager),
+          m_resultsWidget(resultsWidget) {
+
     m_resultsProcessor = new ResultsProcessor();
     m_resultsImporter = new ResultsImporter();
     m_resultsExporter = new ResultsExporter();
 
-    /*
-     * Training Results
-     */
+/*
+ * Training Results
+ */
     auto trainingResultsWidget = m_resultsWidget->getTrainingResultsWidget();
 
-    // Connect 'Top Accuracies Table' related signals/slots
-    connect(trainingResultsWidget, &TrainingResultsWidget::sig_normal_requestTopAccuraciesGraphics,
-            m_resultsProcessor,
-            &ResultsProcessor::slot_normal_generateTopAccuraciesGraphics);
-
-    //Connect signals/slots related to saving top accuracies
-    connect(trainingResultsWidget, &TrainingResultsWidget::sig_save_TopAccuracies,
-            m_resultsExporter,
-            &ResultsExporter::slot_save_TopAccuracies);
-
+    /**
+     * Import functions
+     */
     //Connect 'Compare Run' related signals/slots
     connect(trainingResultsWidget, &TrainingResultsWidget::sig_comparison_loadTrainingResultData,
             m_resultsImporter,
@@ -37,6 +31,9 @@ ResultsController::ResultsController(DataManager *manager, ResultsWidget *result
             m_resultsImporter,
             &ResultsImporter::slot_comparison_unloadAccuracyData);
 
+    /**
+     * Processing functions
+     */
     //Connect signals/slots related to showing training results
     connect(trainingResultsWidget, &TrainingResultsWidget::sig_normal_generateTrainingResultGraphics,
             m_resultsProcessor,
@@ -48,16 +45,31 @@ ResultsController::ResultsController(DataManager *manager, ResultsWidget *result
             m_resultsProcessor,
             &ResultsProcessor::slot_normal_loadTrainingResultData);
 
+    // Connect 'Top Accuracies Table' related signals/slots
+    connect(trainingResultsWidget, &TrainingResultsWidget::sig_normal_requestTopAccuraciesGraphics,
+            m_resultsProcessor,
+            &ResultsProcessor::slot_normal_generateTopAccuraciesGraphics);
+
+    /**
+     * Export functions
+     */
     //Connect signals/slots related to saving training results
     connect(trainingResultsWidget, &TrainingResultsWidget::sig_save_TrainingResult,
             m_resultsExporter,
             &ResultsExporter::slot_save_TrainingResult);
+    //Connect signals/slots related to saving top accuracies
+    connect(trainingResultsWidget, &TrainingResultsWidget::sig_save_TopAccuracies,
+            m_resultsExporter,
+            &ResultsExporter::slot_save_TopAccuracies);
 
 /*
  * Classification Results
  */
     auto classificationResultsWidget = m_resultsWidget->getClassificationResultsWidget();
 
+    /**
+     *Import functions
+     */
     //Connect 'Compare Run' related signals/slots
     connect(classificationResultsWidget, &ClassificationResultsWidget::sig_comparison_loadClassificationResultData,
             m_resultsImporter,
@@ -70,6 +82,10 @@ ResultsController::ResultsController(DataManager *manager, ResultsWidget *result
     connect(m_resultsImporter, &ResultsImporter::sig_normal_loadClassificationResultData,
             m_resultsProcessor,
             &ResultsProcessor::slot_normal_loadClassificationResultData);
+
+    /**
+     * Processing functions
+     */
     connect(classificationResultsWidget, &ClassificationResultsWidget::sig_normal_loadClassificationResultData,
             m_resultsProcessor,
             &ResultsProcessor::slot_normal_loadClassificationResultData);
@@ -77,6 +93,9 @@ ResultsController::ResultsController(DataManager *manager, ResultsWidget *result
             m_resultsProcessor,
             &ResultsProcessor::slot_normal_generateClassificationResultGraphics);
 
+    /**
+     * Export functions
+     */
     //Connect signals/slots related to saving classification results
     connect(classificationResultsWidget, &ClassificationResultsWidget::sig_save_ClassificationResult,
             m_resultsExporter,
@@ -89,12 +108,10 @@ void ResultsController::slot_projectPathUpdated() {
     m_resultsExporter->updateResultFolderPaths();
 }
 
-void ResultsController::slot_addTrainingResult(TrainingResult *result)
-{
+void ResultsController::slot_addTrainingResult(TrainingResult *result) {
     m_resultsWidget->addTrainingResult(result);
 }
 
-void ResultsController::slot_addClassificationResult(ClassificationResult *result)
-{
+void ResultsController::slot_addClassificationResult(ClassificationResult *result) {
     m_resultsWidget->addClassificationResult(result);
 }
