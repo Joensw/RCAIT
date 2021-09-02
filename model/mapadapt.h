8de9fc30eb88ee5ba4672b751af37794ce829b59
Paul S <uqmvi@student.kit.edu>
2021-09-02 22:24:18 +0200

    [Autofix] Make single-argument constructor explicit

--- model/mapadapt.h
+++ model/mapadapt.h
@@ -4,7 +4,7 @@
 template<class Container> class MapAdapt
 {
 public:
-    MapAdapt(const Container& container) : c(container) {}
+    explicit MapAdapt(const Container& container) : c(container) {}
     typename Container::const_key_value_iterator begin() const { return c.keyValueBegin(); }
     typename Container::const_key_value_iterator end() const { return c.keyValueEnd(); }
 private:
