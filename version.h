627e19d3bc32b3c4bd5deb539cdea8d4223e43cf
Paul S <uqmvi@student.kit.edu>
2021-09-03 19:11:59 +0200

    Added application info and icon to exe file (Only works on Windows)

---
+++ version.h
@@ -0,0 +1,23 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define ICON_PATH                   "Resources/RCAIT.ico"
+
+#define VER_FILEVERSION             2,0,0,0
+#define VER_FILEVERSION_STR         "2.0.0.0\0"
+
+#define VER_PRODUCTVERSION          2,0,0,0
+#define VER_PRODUCTVERSION_STR      "2.0\0"
+
+#define VER_COMPANYNAME_STR         "PSE 2021 @ Fraunhofer IOSB"
+#define VER_FILEDESCRIPTION_STR     "Rapid Classification AI Trainer"
+#define VER_INTERNALNAME_STR        "RCAIT"
+#define VER_LEGALCOPYRIGHT_STR       L"\251 2021 (PSE 2021 @ Fraunhofer IOSB)"
+#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    "RCAIT.exe"
+#define VER_PRODUCTNAME_STR         "Rapid Classification AI Trainer"
+
+#define VER_COMPANYDOMAIN_STR       "de.Fraunhofer.IOSB.RCAIT"
+
+#endif // VERSION_H
