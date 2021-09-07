aaf7464003e5f50bc51b03f8f50b0d046e4a68d2
SaschaRolinger <60555506+SaschaRolinger@users.noreply.github.com>
2021-09-08 01:45:49 +0200

    Added test images and wrote test for imageinspectionmodel

--- Google_tests/test_imageinspectionmodel.cpp
+++ Google_tests/test_imageinspectionmodel.cpp
@@ -3,23 +3,36 @@
 #include <QDir>
 
 //check if loading labeled dataset imagefilepaths work
+//with a folder "Mann" and 1 image in dataset
 TEST(ImageInspectionModelTest, testLoadDataSet){
     ImageInspectionModel iim;
     QDir dir(QDir::current().path());
     dir.cd("dataset");
-    iim.loadDataSet(dir.path() + "/Train", dir.path() + "/Validate");
+    iim.loadDataSet(dir.path() + "/training", dir.path() + "/validation");
     EXPECT_TRUE(iim.getTrainDataset().contains("Mann"));
     EXPECT_TRUE(iim.getTrainDataset().value("Mann").isEmpty() == false);
 }
 
-TEST(ImageInspectionModelTest, DemonstratetTestMacros2){
-    ImageInspectionModel img;
+//check if new data is loaded and split correctly
+//use folder with 10 images of car
+//split 40 -> 4 images in validation, 6 in car
+TEST(ImageInspectionModelTest, testLoadNewDataSet){
+    ImageInspectionModel iim;
+    QDir dir(QDir::current().path());
+    dir.cd("temp_dataset");
+    iim.loadNewData(dir.path(),40);
 
-    //EXPECT_TRUE(img.VALIDATION_FOLDER.size() != 0);
+    QMap<QString, QStringList> valData = iim.getValidationNewData();
+    QMap<QString, QStringList> trainData = iim.getTrainNewData();
+
+    EXPECT_TRUE(valData.value("car").size()==4);
+    EXPECT_TRUE(trainData.value("car").size()==6);
+
 }
 
 
 TEST(ImageInspectionModelTest, DemonstratetTestMacros3){
+    ImageInspectionModel iim;
 
 
     EXPECT_TRUE(true);
