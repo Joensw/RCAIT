3aefdc9d11325efc4470f6dcdd8b3a531b197e94
SaschaRolinger <60555506+SaschaRolinger@users.noreply.github.com>
2021-09-08 16:39:03 +0200

    Added documentation to imageinspectionmodel

--- model/imageinspectionmodel.h
+++ model/imageinspectionmodel.h
@@ -18,13 +18,56 @@ private:
     QMap<QString, QStringList> m_validationDataset;
 
 public:
+    /**
+     * @brief loadDataSet loads validation and training data from the given folders. Format in the training/validation folder has to be e.g.
+     * /training
+     *  /Label1
+     *      /image1.jpg
+     *      /image2.jpg
+     *  /Label2
+     *      /image1.jpg
+     *
+     * @param trainingPath the path to training data
+     * @param validationPath the path to validation data
+     */
     void loadDataSet(const QString &trainingPath, const QString &validationPath);
+    /**
+     * @brief loadNewData loads new data from a folder with label subfolders and images there in and splits them by the given percentage
+     * @param path to the folder with the labeled subfolders
+     * @param split as integer (100 = 100%). #validation images = #total images * split (%), and #training images is the rest
+     */
     void loadNewData(const QString &path, int split);
+    /**
+     * @brief mergeDataSets merges the loaded NewData and the dataset specified by the arguments. The files will be moved on the filesystem.
+     * @param trainingPath the path to the training folder
+     * @param validationPath the path to the validation folder
+     */
     void mergeDataSets(const QString &trainingPath, const QString &validationPath);
+    /**
+     * @brief removeImage removes images from ValidationNewData (0), TrainNewData (1), ValidationDataset (2), TrainDataset (3) by label and index in the labeled folder.
+     * @param selectionIndex index of the list where the data is to be deleted
+     * @param removedImages label and indices of the images in the labeled folder
+     */
     void removeImage(int selectionIndex, const QMap<QString, QList<int>>& removedImages);
+    /**
+     * @brief getValidationDataset get the validation dataset used in the model
+     * @return Label as Key and QStringList as paths to images for the given part of the dataset
+     */
     const QMap<QString, QStringList> &getValidationDataset() const;
+    /**
+     * @brief getTrainDataset get the training dataset used in the model
+     * @return Label as Key and QStringList as paths to images for the given part of the dataset
+     */
     const QMap<QString, QStringList> &getTrainDataset() const;
+    /**
+     * @brief getValidationNewData get the new validation data used in the model
+     * @return Label as Key and QStringList as paths to images for the given part of the dataset
+     */
     const QMap<QString, QStringList> &getValidationNewData() const;
+    /**
+     * @brief getTrainNewData get the new training data used in the model
+     * @return Label as Key and QStringList as paths to images for the given part of the dataset
+     */
     const QMap<QString, QStringList> &getTrainNewData() const;
 
 
