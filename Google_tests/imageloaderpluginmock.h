c25691454c3c044f81ee953459adfcc6a934fa8a
SaschaRolinger <60555506+SaschaRolinger@users.noreply.github.com>
2021-09-10 00:33:22 +0200

    Added test suite for imageloaderpluginmanager

---
+++ Google_tests/imageloaderpluginmock.h
@@ -0,0 +1,45 @@
+#ifndef IMAGELOADERPLUGINMOCK_H
+#define IMAGELOADERPLUGINMOCK_H
+#include <QObject>
+#include <QtPlugin>
+#include <QSettings>
+#include <QLineEdit>
+#include <QProcess>
+#include <QProcessEnvironment>
+#include "imageloaderplugin.h"
+#include "progressableplugin.h"
+
+class ImageLoaderPluginMock : public QObject, ImageLoaderPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "de.Fraunhofer.IOSB.RCAIT.FlickrPlugin" FILE "ImageLoaderPluginMock.json")
+    Q_INTERFACES(ImageLoaderPlugin)
+
+public:
+
+    bool loadImages(QString path, ProgressablePlugin* receiver ,int imageCount,  QStringList label) override;
+    QWidget* getConfigurationWidget() override;
+    void saveConfiguration() override;
+    void init() override;
+    QString getName() override;
+    QWidget*  getInputWidget() override;
+    inline static const QString PLUGIN_NAME = "testplugin";
+
+private:
+
+   QWidget *pluginSettings;
+   QScopedPointer<QProcess> m_process;
+   ProgressablePlugin* m_receiver;
+   bool m_success = true;
+
+
+private slots:
+    void slot_abort();
+    void slot_readOutPut();
+    void slot_pluginFinished();
+
+
+
+};
+
+#endif
---
+++ Google_tests/test_imageloaderpluginmanager.cpp
@@ -0,0 +1,47 @@
+#include "imageloaderpluginmanager.h"
+#include "imageloaderpluginmock.h"
+#include <gtest/gtest.h>
+
+
+class ImageLoaderPluginManagerTest : public testing::Test {
+    protected:
+
+
+    static void SetUpTestSuite() {
+
+
+    }
+
+      static void TearDownTestSuite() {
+
+      }
+
+    void SetUp() override {
+        pluginDir = QDir::current().path();
+        testee.loadPlugins(pluginDir);
+    }
+
+    void TearDown() override {
+
+    }
+
+    ImageLoaderPluginManager& testee = ImageLoaderPluginManager::getInstance();
+    QString pluginDir;
+
+ };
+
+
+TEST_F(ImageLoaderPluginManagerTest, testLoadPlugins){
+    QStringList pluginNames = testee.getNamesOfPlugins();
+    EXPECT_TRUE(!pluginNames.isEmpty());
+    EXPECT_TRUE(!QString::compare(pluginNames.at(0), ImageLoaderPluginMock::PLUGIN_NAME));
+}
+
+TEST_F(ImageLoaderPluginManagerTest, testCallMethods){
+    testee.getConfigurationWidget(ImageLoaderPluginMock::PLUGIN_NAME);
+    testee.getInputWidget(ImageLoaderPluginMock::PLUGIN_NAME);
+    testee.getConfigurationWidgets();
+    testee.saveConfiguration(ImageLoaderPluginMock::PLUGIN_NAME);
+    testee.loadImages("",nullptr,ImageLoaderPluginMock::PLUGIN_NAME, 10, QStringList());
+    EXPECT_TRUE(true);
+}
