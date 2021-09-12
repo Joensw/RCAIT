97ae654ff9feca38f447927608a79847320fab0f
Paul S <uqmvi@student.kit.edu>
2021-09-12 19:19:04 +0200

    Added python scripts to get difflib data in a usable format

---
+++ model/utilities/filediff.py
@@ -0,0 +1,29 @@
+import argparse
+import json
+import os
+
+from difflibparser import *
+
+
+def file_path(string):
+    if os.path.isfile(string):
+        return string
+    else:
+        raise FileNotFoundError(string)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Builds a JSON parsable diff from two files')
+    parser.add_argument('file1', metavar='file1', type=file_path, help='First file')
+    parser.add_argument('file2', metavar='file2', type=file_path, help='Second file')
+
+    args = parser.parse_args()
+    file1 = open(args.file1, 'r')
+    file2 = open(args.file2, 'r')
+    file1_content = file1.read()
+    file2_content = file2.read()
+
+    differ = DifflibParser(file1_content.splitlines(), file2_content.splitlines())
+
+    for line in differ:
+        print(json.dumps(line))
