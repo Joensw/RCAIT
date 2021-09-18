ca1f39a2fd43b9be77fbb41e536f0bc4067d9074
AdNe <uevhx@student.kit.edu>
2021-09-18 18:29:24 +0200

    ClassificationPlugin/Manager documentation

--- plugins/classificationplugins/classificationplugin.h
+++ plugins/classificationplugins/classificationplugin.h
@@ -8,18 +8,90 @@
 #include <pluginusage/progressableplugin.h>
 #include "plugin.h"
 
-
+/**
+ * @brief The ClassificationPlugin class is an interface for all classification plugins.
+ *
+ */
 class ClassificationPlugin : public Plugin
 {
 public:
+
+    /**
+     * ClassificationPlugin constructor.
+     *
+     */
     virtual ~ClassificationPlugin() = default;
+
+    /**
+     * @brief getAssociatedModels gets models associeted with plugin.
+     *
+     * @return list of models
+     */
     virtual QStringList getAssociatedModels() = 0;
+
+    /**
+     * @brief createNewModel creates a new model with a name from a base model.
+     *
+     * @param modelName name of new model
+     * @param baseModel base model of new model
+     * @return true if creation was successful
+     */
     virtual bool createNewModel(QString modelName, QString baseModel) = 0;
+
+    /**
+     * @brief getAugmentationPreview loads a preview of augmented images into a folder.
+     *
+     * @param modelName name of model
+     * @param inputPath location of original images
+     * @param targetPath location for new images
+     * @param amount number of augmented images
+     * @return true if successful
+     */
     virtual bool getAugmentationPreview(QString modelName, QString inputPath, QString targetPath, int amount) = 0;
+
+    /**
+     * @brief getDataAugmentationInputWidget gets widget for augmentation config.
+     *
+     * @return data augmentation input widget
+     */
     virtual QWidget* getDataAugmentationInputWidget() = 0;
+
+    /**
+     * @brief removeModel removes model with specified name.
+     *
+     * @param modelName name of model
+     * @return true if removal was successful
+     */
     virtual bool removeModel(QString modelName)= 0;
+
+    /**
+     * @brief train starts a training with the given arguments.
+     *
+     * @param modelName name of model
+     * @param trainDatasetPath location of training images
+     * @param validationDatasetPath location of validation images
+     * @param workingDirectory working directory
+     * @param receiver receives progress of training
+     * @return result data of training
+     */
     virtual TrainingResult* train(QString modelName, QString trainDatasetPath, QString validationDatasetPath, QString workingDirectory, ProgressablePlugin *receiver)= 0;
+
+    /**
+     * @brief classify starts a classification with the given arguments.
+     *
+     * @param inputImageDirPath location of input images
+     * @param trainDatasetPath location of training images
+     * @param workingDirPath working directory
+     * @param modelName name of model
+     * @param receiver receives classification progress
+     * @return result data of classification
+     */
     virtual ClassificationResult* classify(QString inputImageDirPath, QString trainDatasetPath, QString workingDirPath, QString modelName, ProgressablePlugin *receiver)= 0;
+
+    /**
+     * @brief init initializes plugin.
+     *
+     */
     virtual void init() = 0;
 };
 
